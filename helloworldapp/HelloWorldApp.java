44cdd5522d0addf6cdb53aeba31b7838ca0e216a
samanhur <samanghasemidesk@gmail.com>
2025-02-22 07:29:37 +0330

    first commit

---
+++ helloworldapp/HelloWorldApp.java
@@ -0,0 +1,7 @@
+package helloworldapp.helloworldapp;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
